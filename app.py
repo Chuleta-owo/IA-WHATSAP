6c1f21a588ded1fdc3f8c407f19cd5df5bd205e5
Horas & Minutos <71901246+Chuleta-owo@users.noreply.github.com>
2024-09-04 16:49:45 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ import os
 app = Flask(__name__)
 
 # Configura tus credenciales de Twilio
-TWILIO_ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID')
-TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
-TWILIO_PHONE_NUMBER = os.getenv('TWILIO_PHONE_NUMBER')
+TWILIO_ACCOUNT_SID = os.getenv('ACe3b30530004f5f0e00ae549afd10765c')
+TWILIO_AUTH_TOKEN = os.getenv('0bfdac3a779f6958ae8c66d98f6613dd')
+TWILIO_PHONE_NUMBER = os.getenv('14155238886')
 client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
 @app.route('/sms', methods=['POST'])
